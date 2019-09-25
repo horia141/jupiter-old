974ef2e2df8808dd0a125c1b0c81097b275df14e
Horia Coman <horia141@gmail.com>
2019-09-25 23:04:30 +0300

    Actually make the new typescript thing work

--- src/server/index.ts
+++ src/server/index.ts
@@ -10,7 +10,7 @@ function main() {
         client: "pg",
         connection: {
             host: process.env.POSTGRES_HOST as string,
-            port: process.env.POSTGRES_PORT as string,
+            port: Number.parseInt(process.env.POSTGRES_PORT as string),
             database: process.env.POSTGRES_DATABASE as string,
             user: process.env.POSTGRES_USERNAME as string,
             password: process.env.POSTGRES_PASSWORD as string
